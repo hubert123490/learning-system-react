a471715d510eaee1b7de9ba9517a80d99944dde5
hubert123490 <hubert1234.94@gmail.com>
2021-11-17 19:37:28 +0100

    added more routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import "./App.css";
 import Layout from "./components/Layout/Layout";
 import { Routes, Route } from "react-router-dom";
-import AuthForm from "./components/Auth/AuthForm";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import StartingPageContent from "./components/StartingPage/StartingPage";
+import ProfilePage from "./pages/ProfilePage";
+import { useContext } from "react";
+import AuthContext from "./store/auth-context";
+import AuthFormPage from "./pages/AuthFormPage";
 
 function App() {
+  const authCtx = useContext(AuthContext)
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<StartingPageContent />} />
-        <Route path="/auth" element={<AuthForm />} />
+        <Route path="/auth" element={<AuthFormPage />} />
+        {authCtx.isLoggedIn && <Route path="/profile" element={<ProfilePage />} />}
         {/* {<Route path="/*" element={<Navigate to="/" />} />} */}
+        <Route path="*" element={<StartingPageContent />} />
       </Routes>
     </Layout>
   );
