428222f9a72790dd7ac545084c2f45af4c129699
hubert123490 <hubert1234.94@gmail.com>
2021-11-17 20:19:40 +0100

    added protection of routes with roles

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,16 +1,30 @@
 import classes from "./MainNavigation.module.css";
 import { Link } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import AuthContext from "../../store/auth-context";
 
 const MainNavigation = () => {
   const authCtx = useContext(AuthContext);
+  const [user] = useState(JSON.parse(localStorage.getItem("user")));
+  const [showStudentContent, setShowStudentContent] = useState(false);
+  const [showTeacherContent, setShowTeacherContent] = useState(false);
 
   const isLoggedIn = authCtx.isLoggedIn;
 
   const logoutHandler = () => {
     authCtx.logout();
   }
+
+  useEffect(() => {
+    if(user.roles.includes("ROLE_STUDENT")) {
+      setShowStudentContent(true);
+    }
+  
+    if(user.roles.includes("ROLE_TEACHER")) {
+      setShowTeacherContent(true);
+    }
+  }, [user.roles])
+
 
   return (
     <header className={classes.header}>
@@ -21,17 +35,17 @@ const MainNavigation = () => {
         <ul>
           {!isLoggedIn && (
             <li>
-              <Link to="/auth">Login</Link>
+              <Link to="/auth">Logowanie</Link>
+            </li>
+          )}
+          {isLoggedIn && (showStudentContent || showTeacherContent ) && (
+            <li>
+              <Link to="/profile">Profil</Link>
             </li>
           )}
           {isLoggedIn && (
             <li>
-              <Link to="/profile">Profile</Link>
-            </li>
-          )}
-          {isLoggedIn && (
-            <li>
-              <button onClick={logoutHandler}>Logout</button>
+              <button onClick={logoutHandler}>Wyloguj się</button>
             </li>
           )}
         </ul>
