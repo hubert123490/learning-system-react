09b5421de12eb2d2d71ec60fb3763bc6629bb0ad
hubert123490 <hubert1234.94@gmail.com>
2021-11-17 19:39:45 +0100

    added new pages

--- src/components/StartingPage/StartingPage.js
+++ src/components/StartingPage/StartingPage.js
@@ -1,22 +1,42 @@
 import classes from "./StartingPage.module.css";
 import useHttp from "../../hooks/use-http";
-import { studentTest } from "../../lib/test-api";
-import authHeader from "../../lib/auth-header";
+import { getAllCourses } from "../../lib/course-api";
+import { useEffect } from "react";
+import LoadingSpinner from "../UI/LoadingSpinner";
+import Card from "../UI/Card";
 
 const StartingPageContent = () => {
-  const { sendRequest } = useHttp(studentTest);
+  const { sendRequest, data, error, status } = useHttp(getAllCourses, true);
 
-  const testFunction = (event) => {
-    event.preventDefault();
-    sendRequest(authHeader());
+  const testFunc = () => {
+    console.log("test");
   };
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  console.log(data);
 
   return (
     <section className={classes.starting}>
       <h1>Welcome on Board!</h1>
-      <form onSubmit={testFunction}>
-        <button type="submit">Submit</button>
-      </form>
+      {data ? (
+        data.courses.map((item) => (
+          <div key={item.id} onClick={testFunc} className={classes.course}>
+            <Card >
+              <h2>{item.name}</h2>
+              <div>{item.category}</div>
+            </Card>
+          </div>
+        ))
+      ) : (
+        <Card>
+          <div>
+            <LoadingSpinner />
+          </div>
+        </Card>
+      )}
     </section>
   );
 };
---
+++ src/pages/AuthFormPage.js
@@ -0,0 +1,7 @@
+import AuthForm from "../components/Auth/AuthForm";
+
+const AuthFormPage = () => {
+    return <AuthForm />
+}
+
+export default AuthFormPage;
