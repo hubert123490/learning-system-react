16ea886b99dee1c6091be622573b86d25f5f3b6f
hubert123490 <hubert1234.94@gmail.com>
2021-11-17 19:39:16 +0100

    small changes like logo name, styling

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -15,7 +15,7 @@ const MainNavigation = () => {
   return (
     <header className={classes.header}>
       <Link to="/">
-        <div className={classes.logo}>React Auth</div>
+        <div className={classes.logo}>Bubex</div>
       </Link>
       <nav>
         <ul>
