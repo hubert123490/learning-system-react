61976446aa114c833a7314f95c9ce699d478ffed
hubert123490 <hubert1234.94@gmail.com>
2021-11-19 12:38:07 +0100

    fixes in StartingPage (issue with keys)

--- src/components/StartingPage/StartingPage.js
+++ src/components/StartingPage/StartingPage.js
@@ -6,27 +6,40 @@ import LoadingSpinner from "../UI/LoadingSpinner";
 import Card from "../UI/Card";
 
 const StartingPageContent = () => {
-  const { sendRequest, data, error, status } = useHttp(getAllCourses, true);
+  const { sendRequest, data, } = useHttp(getAllCourses, true);
 
   const testFunc = () => {
     console.log("test");
   };
 
+  const getTitle = title => {
+    if(title === "Bachelor")
+      return "Inżynier";
+    else if (title === "Master")
+      return "Doktor"
+  }
+
   useEffect(() => {
     sendRequest();
+    return sendRequest
   }, [sendRequest]);
 
   console.log(data);
 
   return (
     <section className={classes.starting}>
-      <h1>Welcome on Board!</h1>
+      <h1>Aktualnie prowadzone kursy!</h1>
       {data ? (
         data.courses.map((item) => (
           <div key={item.id} onClick={testFunc} className={classes.course}>
             <Card >
               <h2>{item.name}</h2>
+              <div>Kategoria kursu:</div>
               <div>{item.category}</div>
+              <div>Prowadzony przez: </div>
+              {item.person.map((person) => (
+                <div key={person.id}>{getTitle(person.title)} {person.firstName} {person.lastName}</div>
+              ))}
             </Card>
           </div>
         ))
