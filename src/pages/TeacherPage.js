4e002007b283ae68e3e0e196ba6ffdb358866f59
hubert123490 <hubert1234.94@gmail.com>
2021-11-23 17:08:32 +0100

    improved Routes in TeacherPage

--- src/pages/TeacherPage.js
+++ src/pages/TeacherPage.js
@@ -1,14 +1,19 @@
-import { Route, Routes } from "react-router";
-import CreateCourse from "../components/Teacher/Courses/CreateCourse";
 import TeacherPanel from "../components/Teacher/TeacherPanel";
+import { Route, Routes } from "react-router-dom";
+import MyCourses from "../components/Teacher/Courses/MyCourses";
+import CourseDetails from "../components/Teacher/Courses/CourseDetails";
+import LessonDetails from "../components/Teacher/Lessons/LessonDetails";
 
 const TeacherPage = () => {
-  return (<>
-    <TeacherPanel />
+  return (
     <Routes>
-      <Route path="/create-course" element={<CreateCourse />} />
+      <Route path="/courses/:courseId/lessons/:lessonId/*" element={<LessonDetails />} />
+      <Route path="/courses/:courseId/*" element={<CourseDetails />} />
+      <Route path="/my-courses" element={<MyCourses />} />
+      <Route path="/" element={<TeacherPanel />} />
+     
+      
     </Routes>
-    </>
   );
 };
 
