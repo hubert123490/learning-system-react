7a03a35d8cff201266cfca3133579855be871968
hubert123490 <hubert1234.94@gmail.com>
2021-11-29 01:08:07 +0100

    new route for making exams

--- src/pages/TeacherPage.js
+++ src/pages/TeacherPage.js
@@ -3,15 +3,16 @@ import { Route, Routes } from "react-router-dom";
 import MyCourses from "../components/Teacher/Courses/MyCourses";
 import CourseDetails from "../components/Teacher/Courses/CourseDetails";
 import LessonDetails from "../components/Teacher/Lessons/LessonDetails";
+import ExamDetails from "../components/Teacher/Exams/ExamDetails";
 
 const TeacherPage = () => {
   return (
     <Routes>
       <Route path="/courses/:courseId/lessons/:lessonId/*" element={<LessonDetails />} />
+      <Route path="/courses/:courseId/exams/:examId/*" element={<ExamDetails />} />
       <Route path="/courses/:courseId/*" element={<CourseDetails />} />
       <Route path="/my-courses" element={<MyCourses />} />
       <Route path="/" element={<TeacherPanel />} />
-     
       
     </Routes>
   );
