b820139ec8ff5f157bd65b8f07c7f346152c7978
hubert123490 <hubert1234.94@gmail.com>
2021-11-30 21:26:48 +0100

    added routes for checking exams

--- src/pages/TeacherPage.js
+++ src/pages/TeacherPage.js
@@ -4,16 +4,24 @@ import MyCourses from "../components/Teacher/Courses/MyCourses";
 import CourseDetails from "../components/Teacher/Courses/CourseDetails";
 import LessonDetails from "../components/Teacher/Lessons/LessonDetails";
 import ExamDetails from "../components/Teacher/Exams/ExamDetails";
+import CheckSubmissions from "../components/Teacher/Submissions/CheckSubmissions";
+import CheckSubmissionDetails from "../components/Teacher/Submissions/CheckSubmissionDetails";
+import CheckExams from "../components/Teacher/Exams/CheckExams";
+import CheckCourses from "../components/Teacher/Courses/CheckCourses";
 
 const TeacherPage = () => {
   return (
     <Routes>
+      <Route path="/check-courses/:courseId/exams/:examId/submissions/:submissionId/*" element={<CheckSubmissionDetails />} />
+      <Route path="/check-courses/:courseId/exams/:examId/submissions/*" element={<CheckSubmissions />} />
+      <Route path="/check-courses/:courseId/exams/*" element={<CheckExams />} />
+      <Route path="/check-courses/*" element={<CheckCourses />} />
+      <Route path="/courses/:courseId/exams/:examId/submissions/*" element={<CheckSubmissions />} />
       <Route path="/courses/:courseId/lessons/:lessonId/*" element={<LessonDetails />} />
       <Route path="/courses/:courseId/exams/:examId/*" element={<ExamDetails />} />
       <Route path="/courses/:courseId/*" element={<CourseDetails />} />
       <Route path="/my-courses" element={<MyCourses />} />
       <Route path="/" element={<TeacherPanel />} />
-      
     </Routes>
   );
 };
