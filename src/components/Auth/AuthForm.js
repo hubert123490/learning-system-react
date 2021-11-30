192d4b54a37d9e8478e31efc25fe20df7efb7712
hubert123490 <hubert1234.94@gmail.com>
2021-11-30 21:25:44 +0100

    rework of signup side

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,6 +1,6 @@
 import React, { useState, useContext, useEffect } from "react";
 import useForm from "../../hooks/use-form";
-import { signinForm, signupForm } from "../../lib/forms/auth-form"
+import { signinForm, signupForm } from "../../lib/forms/auth-form";
 import useHttp from "../../hooks/use-http";
 import { signUp, signIn } from "../../lib/api/auth-api";
 import LoadingSpinner from "../UI/LoadingSpinner";
@@ -62,91 +62,121 @@ export default function AuthForm() {
       });
       if (signInData) {
         authCtx.login(signInData.token);
-        
       }
     } else {
       if (!isSignupFormValid) {
         return;
       }
+      let role = systemRole === "student" ? "student" : systemRole;
       signUpRequest({
         firstName: renderSignupFormInputs()[0].props.value,
         lastName: renderSignupFormInputs()[1].props.value,
         email: renderSignupFormInputs()[2].props.value,
         password: renderSignupFormInputs()[4].props.value,
-        title: degree,
-        role: [systemRole],
+        title: role === "student" ? "Student" : degree,
+        role: [role],
       });
     }
   };
 
   const handleDegreeChange = (event) => {
     setDegree(event.target.value);
-  }
+  };
 
   const handleSystemRoleChange = (event) => {
     setSystemRole(event.target.value);
-  }
+  };
 
   const signupFormValidation = () => {
-    return (systemRole !== "" && degree !== "" && isSignupFormValid())
-  }
+    if (systemRole === "student" && isSignupFormValid()) return true;
+    else if (systemRole !== "" && degree !== "" && isSignupFormValid())
+      return true;
+    else return false;
+  };
 
   return (
     <>
-    
-    <form className={classes.signupForm} onSubmit={submitHandler}>
-      <h1>{isLogin ? "Logowanie" : "Rejestracja"}</h1>
-      {!isLogin && signUpData ? <h1 className={classes.success}>User Registered Successfully</h1> : <>
-      {isLogin && renderSigninFormInputs()}
-      {!isLogin && renderSignupFormInputs()}
-      {!isLogin && (
-        <div>
-        <label htmlFor="role-select">Wybierz rolę w systemie:</label>
-        <Form.Select value={systemRole} onChange={handleSystemRoleChange} name="roles" id="role-select" required>
-          <option value="">Wybierz rolę w systemie</option>
-          <option value="student">STUDENT</option>
-          <option value="teacher">NAUCZYCIEL</option>
-        </Form.Select>
-        <br />
-        <label htmlFor="degree-select">Wybierz tytuł naukowy:</label>
-        <Form.Select value={degree} onChange={handleDegreeChange} name="degrees" id="degree-select" required>
-          <option value="">Wybierz tytuł naukowy:</option>
-          {systemRole === "student" &&<option value="Student">Student</option>}
-          <option value="Bachelor">Inżynier</option>
-          <option value="Master">Magister</option>
-          <option value="Professor">Profesor</option>
-        </Form.Select>
-        <br/>
-        </div>
-      )}
-      {isLogin && signInError && <div className={classes.error}>{signInError}</div>}
-      {/* {!isLogin && signUpData && <div className={classes.success}>{signUpData.message}</div>} */}
-      {!isLogin && signUpError && <div className={classes.error}>{signUpError}</div>}
-      
-      {signInStatus !== "pending" && isLogin && (
-        <button type="submit" disabled={!isSigninFormValid()}>
-          {isLogin ? "Zaloguj się" : "Stwórz konto"}
-        </button>
-      )}
-      {signUpStatus !== "pending" && !isLogin && (
-        <button type="submit" disabled={!signupFormValidation()}>
-          {isLogin ? "Zaloguj się" : "Stwórz konto"}
-        </button>
-      )}
+      <form className={classes.signupForm} onSubmit={submitHandler}>
+        <h1>{isLogin ? "Logowanie" : "Rejestracja"}</h1>
+        {!isLogin && signUpData ? (
+          <h3 className={classes.success}>Pomyślnie stworzono nowe konto</h3>
+        ) : (
+          <>
+            {isLogin && renderSigninFormInputs()}
+            {!isLogin && renderSignupFormInputs()}
+            {!isLogin && (
+              <div>
+                <label htmlFor="role-select">Wybierz rolę w systemie:</label>
+                <Form.Select
+                  value={systemRole}
+                  onChange={handleSystemRoleChange}
+                  name="roles"
+                  id="role-select"
+                  required
+                >
+                  <option value="">Wybierz rolę w systemie</option>
+                  <option value="student">STUDENT</option>
+                  <option value="teacher">NAUCZYCIEL</option>
+                </Form.Select>
+                <br />
+                {systemRole === "teacher" && (
+                  <>
+                    <label htmlFor="degree-select">
+                      Wybierz tytuł naukowy:
+                    </label>
+                    <Form.Select
+                      value={degree}
+                      onChange={handleDegreeChange}
+                      name="degrees"
+                      id="degree-select"
+                      required
+                    >
+                      <option value="">Wybierz tytuł naukowy:</option>
+                      {/* <option value="Student">Student</option> */}
+                      <option value="Bachelor">Inżynier</option>
+                      <option value="Master">Magister</option>
+                      <option value="Professor">Profesor</option>
+                    </Form.Select>
+                  </>
+                )}
+                <br />
+              </div>
+            )}
+            {isLogin && signInError && (
+              <div className={classes.error}>{signInError}</div>
+            )}
+            {!isLogin && signUpError && (
+              <div className={classes.error}>{signUpError}</div>
+            )}
 
-      {signUpStatus === "pending" ||
-        signInStatus === "pending" ? <LoadingSpinner /> : ""}
-        </>}
-      <div className={classes.actions}>
-        <button
-          type="button"
-          onClick={switchAuthModeHandler}
-          style={{ width: "100%", marginTop: "1em" }}
-        >
-          {isLogin ? "Stwórz nowe konto" : "Zaloguj się"}
-        </button>
-      </div>
-    </form>
+            {signInStatus !== "pending" && isLogin && (
+              <button type="submit" disabled={!isSigninFormValid()}>
+                {isLogin ? "Zaloguj się" : "Stwórz konto"}
+              </button>
+            )}
+            {signUpStatus !== "pending" && !isLogin && (
+              <button type="submit" disabled={!signupFormValidation()}>
+                {isLogin ? "Zaloguj się" : "Stwórz konto"}
+              </button>
+            )}
+
+            {signUpStatus === "pending" || signInStatus === "pending" ? (
+              <LoadingSpinner />
+            ) : (
+              ""
+            )}
+          </>
+        )}
+        <div className={classes.actions}>
+          <button
+            type="button"
+            onClick={switchAuthModeHandler}
+            style={{ width: "100%", marginTop: "1em" }}
+          >
+            {isLogin ? "Stwórz nowe konto" : "Zaloguj się"}
+          </button>
+        </div>
+      </form>
     </>
   );
 }
