930dd7545bb7ac06d8e3fbaee17a4cd53a1d255b
hubert123490 <hubert1234.94@gmail.com>
2021-12-05 20:07:11 +0100

    improved submission check

--- src/components/Student/Submission.js
+++ src/components/Student/Submission.js
@@ -42,13 +42,19 @@ const Submission = () => {
     })
   }
 
+  console.log(checkSubmissionData)
+
   return <div className={classes.submission}>
       <Card>
           <div>
               {<h1>Witaj!</h1>}
+              {checkSubmissionData && checkSubmissionData.status === "SUCCESS" && <div>Egzamin zaczyna się: {checkSubmissionData.startDate.split("T")[0]} o godzinie {checkSubmissionData.startDate.split("T")[1]}</div>}<br/>
+              {makeSubmissionData && makeSubmissionData.status === "ERROR" && <div className={classes.error}><h3>{makeSubmissionData.message}</h3></div>}
               {checkSubmissionData && checkSubmissionData.status === "SUCCESS" && <form onSubmit={makeSubmissionHandler}><button>Przystąp do egzaminu</button></form>}
               {checkSubmissionData && checkSubmissionData.status === "ERROR" && <div><h3>Test zakończony pomyślnie</h3><div>{checkSubmissionData.message}</div></div>}
               {checkSubmissionData && checkSubmissionData.status === "PENDING" && <div><h3>Test jest w trakcie sprawdzania</h3><div>{checkSubmissionData.message}</div></div>}
+              {checkSubmissionData && checkSubmissionData.status === "CHECKING" && <div><h3>Test jest w trakcie sprawdzania</h3><div>{checkSubmissionData.message}</div></div>}
+              {checkSubmissionData && checkSubmissionData.status === "ENDED" && <div><h3>Test zakończył się</h3><div>{checkSubmissionData.message}</div></div>}
           </div>
       </Card>
   </div>
