6e71b4206811901e23f3fdb3bd34895d9f0ac13c
hubert123490 <hubert1234.94@gmail.com>
2021-12-05 20:06:28 +0100

    added date fields to exam form

--- src/lib/forms/exam-form.js
+++ src/lib/forms/exam-form.js
@@ -2,15 +2,31 @@ import { createFormFieldConfig } from "../../utils/formConig";
 import { requiredRule, minLengthRule } from "../../utils/inputValidationRules";
 
 export const createExamForm = {
-    examName: {
-      ...createFormFieldConfig("Nazwa egzaminu", "examName", "text"),
-      validationRules: [
-        requiredRule("Nazwa egzaminu"),
-        minLengthRule("Nazwa egzaminu", 3),
-      ],
-    },
-    examDescription: {
-        ...createFormFieldConfig("Opis egzaminu", "examDescription", "text"),
-        validationRules: []
-    }
-  };
+  examName: {
+    ...createFormFieldConfig("Nazwa egzaminu", "examName", "text"),
+    validationRules: [
+      requiredRule("Nazwa egzaminu"),
+      minLengthRule("Nazwa egzaminu", 3),
+    ],
+  },
+  examDescription: {
+    ...createFormFieldConfig("Opis egzaminu", "examDescription", "text"),
+    validationRules: [],
+  },
+  startDate: {
+    ...createFormFieldConfig(
+      "Czas rozpoczęcia egzaminu",
+      "startDate",
+      "datetime-local"
+    ),
+    validationRules: [requiredRule("Czas rozpoczęcia")],
+  },
+  endDate: {
+    ...createFormFieldConfig(
+      "Czas zakończenia egzaminu",
+      "endDate",
+      "datetime-local"
+    ),
+    validationRules: [requiredRule("Czas zakończenia")],
+  },
+};
