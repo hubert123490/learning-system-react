692bb9fc47fd66069af303d9104ac5dcdac9c549
hubert123490 <hubert1234.94@gmail.com>
2021-12-05 20:05:11 +0100

    route for students grades

--- src/pages/TeacherPage.js
+++ src/pages/TeacherPage.js
@@ -8,10 +8,14 @@ import CheckSubmissions from "../components/Teacher/Submissions/CheckSubmissions
 import CheckSubmissionDetails from "../components/Teacher/Submissions/CheckSubmissionDetails";
 import CheckExams from "../components/Teacher/Exams/CheckExams";
 import CheckCourses from "../components/Teacher/Courses/CheckCourses";
+import GradeCourses from "../components/Teacher/Grades/GradeCourses";
+import GradeTable from "../components/Teacher/Grades/GradeTable";
 
 const TeacherPage = () => {
   return (
     <Routes>
+       <Route path="/grades/:courseId/*" element={<GradeTable />} />
+      <Route path="/grades/*" element={<GradeCourses />} />
       <Route path="/check-courses/:courseId/exams/:examId/submissions/:submissionId/*" element={<CheckSubmissionDetails />} />
       <Route path="/check-courses/:courseId/exams/:examId/submissions/*" element={<CheckSubmissions />} />
       <Route path="/check-courses/:courseId/exams/*" element={<CheckExams />} />
