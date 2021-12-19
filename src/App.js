183bd1ba841cf06c38c7a4cb9b451f2399d6cd72
hubert123490 <hubert1234.94@gmail.com>
2021-12-19 01:14:57 +0100

    improvements

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Layout from "./components/Layout/Layout";
 import { Routes, Route } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import StartingPageContent from "./components/StartingPage/StartingPage";
 import ProfilePage from "./pages/ProfilePage";
 import { useContext, useState, useEffect } from "react";
@@ -10,28 +10,26 @@ import AuthFormPage from "./pages/AuthFormPage";
 import TeacherPage from "./pages/TeacherPage";
 import StudentPage from "./pages/StudentPage";
 
-
 function App() {
-  const authCtx = useContext(AuthContext)
+  const authCtx = useContext(AuthContext);
 
   const [user] = useState(JSON.parse(localStorage.getItem("user")));
   const [showStudentContent, setShowStudentContent] = useState(false);
   const [showTeacherContent, setShowTeacherContent] = useState(false);
 
-  
   const isLoggedIn = authCtx.isLoggedIn;
 
   useEffect(() => {
-    if(user) {
-    if(user.roles.includes("ROLE_STUDENT")) {
-      setShowStudentContent(true);
+    if (user) {
+      if (user.roles.includes("ROLE_STUDENT")) {
+        setShowStudentContent(true);
+      }
+
+      if (user.roles.includes("ROLE_TEACHER")) {
+        setShowTeacherContent(true);
+      }
     }
-  
-    if(user.roles.includes("ROLE_TEACHER")) {
-      setShowTeacherContent(true);
-    }
-  }
-  }, [user])
+  }, [user]);
 
   return (
     <Layout>
@@ -39,9 +37,12 @@ function App() {
         <Route path="/" element={<StartingPageContent />} />
         <Route path="/auth" element={<AuthFormPage />} />
         {isLoggedIn && <Route path="/profile" element={<ProfilePage />} />}
-        {isLoggedIn  && authCtx.isTeacher && <Route path="/teacher/*" element={<TeacherPage />} />}
-        {isLoggedIn  && (authCtx.isTeacher || authCtx.isStudent) && <Route path="/student/*" element={<StudentPage />} />}
-        
+        {isLoggedIn && authCtx.isTeacher && (
+          <Route path="/teacher/*" element={<TeacherPage />} />
+        )}
+        {isLoggedIn && (authCtx.isTeacher || authCtx.isStudent) && (
+          <Route path="/student/*" element={<StudentPage />} />
+        )}
         <Route path="*" element={<AuthFormPage />} />
       </Routes>
     </Layout>
