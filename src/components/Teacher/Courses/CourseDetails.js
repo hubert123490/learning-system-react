11c2c57ceafee313ee01cf1e1ecdaeb68297d67a
hubert123490 <hubert1234.94@gmail.com>
2021-12-19 01:17:23 +0100

    better structure and details - teacher courses

--- src/components/Teacher/Courses/CourseDetails.js
+++ src/components/Teacher/Courses/CourseDetails.js
@@ -6,17 +6,21 @@ import Card from "../../UI/Card";
 import LoadingSpinner from "../../UI/LoadingSpinner";
 import { createLesson, deleteLesson } from "../../../lib/api/lesson-api";
 import { createExam, deleteExam } from "../../../lib/api/exam-api";
-import { useNavigate, useLocation, useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import { createLessonForm } from "../../../lib/forms/lesson-form";
 import useForm from "../../../hooks/use-form";
 import { createExamForm } from "../../../lib/forms/exam-form";
+import { FaTimes } from "react-icons/fa";
+import lessonImage from "../../../assets/lesson.jpg";
+import examImage from "../../../assets/exam.jpg";
 
 const CourseDetails = () => {
   const navigate = useNavigate();
-  const location = useLocation();
   const params = useParams();
   const [showCreateLesson, setShowCreateLesson] = useState(false);
   const [showCreateExam, setShowCreateExam] = useState(false);
+  const [showLessons, setShowLessons] = useState(true);
+  const [showExams, setShowExams] = useState(true);
   const {
     renderFormInputs: renderCreateLessonFormInputs,
     isFormValid: isCreateLessonFormValid,
@@ -34,7 +38,7 @@ const CourseDetails = () => {
   } = useHttp(courseDetails, true);
   const { sendRequest: deleteLessonRequest, status: deleteLessonStatus } =
     useHttp(deleteLesson);
-    const {sendRequest : deleteExamRequest, status: deleteExamStatus } =
+  const { sendRequest: deleteExamRequest, status: deleteExamStatus } =
     useHttp(deleteExam);
   const {
     sendRequest: createLessonRequest,
@@ -60,21 +64,22 @@ const CourseDetails = () => {
     deleteLessonStatus,
     deleteExamStatus,
     createdLessonStatus,
-    createdExamStatus
+    createdExamStatus,
   ]);
 
   const lessonDetailsHandler = (lessonId) => {
-    if (location.pathname) navigate(`${location.pathname}/lessons/${lessonId}`);
+    navigate(`lessons/${lessonId}`);
   };
 
   const examDetailsHandler = (examId) => {
-    if(location.pathname) navigate(`${location.pathname}/exams/${examId}`)
-  }
+    navigate(`exams/${examId}`);
+  };
 
   const submitCreateFormHandler = (event) => {
     event.preventDefault();
     createLessonRequest({
       name: renderCreateLessonFormInputs()[0].props.value,
+      description: renderCreateLessonFormInputs()[1].props.value,
       courseId: params.courseId,
     });
   };
@@ -91,36 +96,49 @@ const CourseDetails = () => {
   };
 
   const showCreateFormHandler = () => {
-    setShowCreateLesson(!showCreateLesson);
+    setShowCreateLesson((prevState) => {
+      return !prevState;
+    });
   };
 
   const showCreateExamFormHandler = () => {
-    setShowCreateExam(!showCreateExam);
+    setShowCreateExam((prevState) => {
+      return !prevState;
+    });
   };
 
   const createLessonRedirectHandler = () => {
-    navigate(`${location.pathname}/lessons/${createdLessonData.id}`);
+    navigate(`lessons/${createdLessonData.id}`);
   };
 
   const createExamRedirectHandler = () => {
-    navigate(`${location.pathname}/exams/${createdExamData.id}`);
-  }
+    navigate(`exams/${createdExamData.id}`);
+  };
+
+  const showExamsHandler = () => {
+    setShowExams((prevState) => {
+      return !prevState;
+    });
+  };
+
+  const showLessonsHandler = () => {
+    setShowLessons((prevState) => {
+      return !prevState;
+    });
+  };
 
   const createLessonFormJSX = (
-    <form
-      className={classes.createLessonForm}
-      onSubmit={submitCreateFormHandler}
-    >
+    <form className={classes["create-form"]} onSubmit={submitCreateFormHandler}>
       {!createdLessonData && <h1>Stwórz nową lekcję</h1>}
       {createdLessonData && <h1>Utworzono lekcję</h1>}
       {!createdLessonData && renderCreateLessonFormInputs()}
       {createdLessonStatus === "completed" && createdLessonError ? (
-        <div className={classes.error}>{createdLessonError}</div>
+        <div className={classes["error"]}>{createdLessonError}</div>
       ) : (
         ""
       )}
       {createdLessonStatus === "completed" && createdLessonData ? (
-        <div className={classes.success}>{createdLessonData.message}</div>
+        <div className={classes["success"]}>{createdLessonData.message}</div>
       ) : (
         ""
       )}
@@ -142,19 +160,19 @@ const CourseDetails = () => {
 
   const createExamFormJSX = (
     <form
-      className={classes.createLessonForm}
+      className={classes["create-form"]}
       onSubmit={submitCreateExamFormHandler}
     >
       {!createdExamData && <h1>Stwórz nowy egzamin</h1>}
       {createdExamData && <h1>Utworzono egzamin</h1>}
       {!createdExamData && renderCreateExamFormInputs()}
       {createdExamStatus === "completed" && createdExamError ? (
-        <div className={classes.error}>{createdExamError}</div>
+        <div className={classes["error"]}>{createdExamError}</div>
       ) : (
         ""
       )}
       {createdExamStatus === "completed" && createdExamData ? (
-        <div className={classes.success}>{createdExamData.message}</div>
+        <div className={classes["success"]}>{createdExamData.message}</div>
       ) : (
         ""
       )}
@@ -175,10 +193,10 @@ const CourseDetails = () => {
   );
 
   return (
-    <div className={classes.courseDetails}>
+    <div className={classes["course-details"]}>
       <h1 style={{ fontSize: "3rem" }}>Witaj!</h1>
 
-      <section className={classes.courseDetails}>
+      <section className={classes["course-details"]}>
         <div>
           <button onClick={showCreateFormHandler}>
             {showCreateLesson ? "Zamknij" : "Dodaj lekcję"}
@@ -193,20 +211,28 @@ const CourseDetails = () => {
           {showCreateExam && createExamFormJSX}
         </div>
         <hr />
-        {getDetailsData &&
-          getDetailsStatus === "completed" &&
-          getDetailsData.lessons.map((item) => (
-            <div key={item.id} className={classes.course}>
-              <div className={classes.card}>
-                <div
-                  onClick={() => {
-                    lessonDetailsHandler(item.id);
-                  }}
-                >
-                  <h2>{item.name}</h2>
-                </div>
-
-                <button
+        <div className={classes["filter-container"]}>
+          <button
+            onClick={showLessonsHandler}
+            className={!showLessons ? classes["button-active"] : ""}
+          >
+            {showLessons ? "Ukryj lekcje" : "Pokaż lekcje"}
+          </button>
+          <button
+            onClick={showExamsHandler}
+            className={!showExams ? classes["button-active"] : ""}
+          >
+            {showExams ? "Ukryj egzaminy" : "Pokaż egzaminy"}
+          </button>
+        </div>
+        <div className={classes["content-container"]}>
+          {getDetailsData &&
+            showLessons &&
+            getDetailsStatus === "completed" &&
+            getDetailsData.lessons.map((item) => (
+              <div key={item.id} className={classes["content-container__item"]}>
+                <FaTimes
+                  size={25}
                   onClick={() => {
                     if (
                       window.confirm(`Czy napewno chcesz usunąć ${item.name}?`)
@@ -216,25 +242,40 @@ const CourseDetails = () => {
                         lessonId: item.id,
                       });
                   }}
-                >
-                  Usuń lekcję
-                </button>
-              </div>
-            </div>
-          ))}
-          {getDetailsData && getDetailsStatus === "completed" && 
-           getDetailsData.exams.map((item) => (
-            <div key={item.id} className={classes.course}>
-              <div className={classes.card} style={{backgroundColor : "lightyellow"}}>
+                  className={classes["delete-icon"]}
+                />
+
                 <div
                   onClick={() => {
-                    examDetailsHandler(item.id);
+                    lessonDetailsHandler(item.id);
                   }}
                 >
-                  <h2>{item.name}</h2>
+                  <h2 className={classes["content-container__title"]}>
+                    {item.name}
+                  </h2>
+                  <div
+                    className={classes["content-container__image-container"]}
+                  >
+                    <img src={lessonImage} alt="image" />
+                  </div>
+                  <div className={classes["content-container__description"]}>{item.description}</div>
                 </div>
-
-                <button onClick={() => {
+              </div>
+            ))}
+        </div>
+        <hr className={classes["content-container__horizontal"]} />
+        <div className={classes["content-container"]}>
+          {getDetailsData &&
+            showExams &&
+            getDetailsStatus === "completed" &&
+            getDetailsData.exams.map((item) => (
+              <div
+                key={item.id}
+                className={`${classes["content-container__item"]} ${classes["exam"]}`}
+              >
+                <FaTimes
+                  size={25}
+                  onClick={() => {
                     if (
                       window.confirm(`Czy napewno chcesz usunąć ${item.name}?`)
                     )
@@ -242,12 +283,27 @@ const CourseDetails = () => {
                         courseId: params.courseId,
                         examId: item.id,
                       });
-                  }}>
-                  Usuń egzamin
-                </button>
+                  }}
+                  className={classes["delete-icon"]}
+                />
+                <div
+                  onClick={() => {
+                    examDetailsHandler(item.id);
+                  }}
+                >
+                  <h2 className={classes["content-container__title"]}>
+                    {item.name}
+                  </h2>
+                  <div
+                    className={classes["content-container__image-container"]}
+                  >
+                    <img src={examImage} alt="image" />
+                  </div>
+                  <div className={classes["content-container__description"]}>{item.description}</div>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+        </div>
         {getDetailsStatus !== "completed" && !getDetailsData && (
           <Card>
             <div>
@@ -256,7 +312,7 @@ const CourseDetails = () => {
           </Card>
         )}
         {getDetailsError && (
-          <div className={classes.error}>{getDetailsError}</div>
+          <div className={classes["error"]}>{getDetailsError}</div>
         )}
         {!getDetailsData && getDetailsStatus === "completed" && <h2>Pusto</h2>}
       </section>
--- src/components/Teacher/Courses/MyCourses.js
+++ src/components/Teacher/Courses/MyCourses.js
@@ -11,18 +11,20 @@ import Card from "../../UI/Card";
 import { useNavigate } from "react-router";
 import useForm from "../../../hooks/use-form";
 import { createCourseForm } from "../../../lib/forms/course-form";
+import image from "./../../../assets/course_image.jpg";
+import { FaTimes } from "react-icons/fa";
 
 const MyCourses = () => {
   const navigate = useNavigate();
   const [showCreateForm, setShowCreateForm] = useState(false);
   const {
-    renderFormInputs: renderCreateFormInputs,
-    isFormValid: isCreateFormValid,
+    renderFormInputs: renderCreateFormInputs
   } = useForm(createCourseForm);
-  const { sendRequest: getAllCourses, data: coursesData, error : courseError } = useHttp(
-    getMyCourses,
-    true
-  );
+  const {
+    sendRequest: getAllCourses,
+    data: coursesData,
+    error: courseError,
+  } = useHttp(getMyCourses, true);
   const {
     sendRequest: deleteCourseRequest,
     // data: deleteData,
@@ -56,16 +58,18 @@ const MyCourses = () => {
   };
 
   const createCourseRedirectHandler = () => {
-    navigate(`/teacher/courses/${createdData.id}`)
-  }
+    navigate(`/teacher/courses/${createdData.id}`);
+  };
 
   const showCreateFormHandler = () => {
-    setShowCreateForm(!showCreateForm)
-  }
-
+    setShowCreateForm(!showCreateForm);
+  };
 
   const createForm = (
-    <form className={classes.createCourseForm} onSubmit={submitCreateFormHandler}>
+    <form
+      className={classes["create-course__form"]}
+      onSubmit={submitCreateFormHandler}
+    >
       {!createdData && <h1>Stwórz nowy kurs</h1>}
       {createdData && <h1>Utworzono kurs</h1>}
       {!createdData && renderCreateFormInputs()}
@@ -81,53 +85,101 @@ const MyCourses = () => {
       )}
       {createdStatus === "pending" && !createdData && <LoadingSpinner />}
       {!createdData && createdStatus !== "pending" && (
-        <button type="submit" disabled={!(renderCreateFormInputs()[0].props.isValid && renderCreateFormInputs()[1].props.isValid)}>
+        <button
+          type="submit"
+          disabled={
+            !(
+              renderCreateFormInputs()[0].props.isValid &&
+              renderCreateFormInputs()[1].props.isValid
+            )
+          }
+        >
           Stwórz kurs{" "}
         </button>
       )}
       {createdStatus === "completed" && createdData && (
-        <button onClick={createCourseRedirectHandler}>Przejdź do nowo utworzonego kursu</button>
+        <button onClick={createCourseRedirectHandler}>
+          Przejdź do nowo utworzonego kursu
+        </button>
       )}
     </form>
   );
 
   return (
-    <section className={classes.myCourses}>
-      <h1>Twoje kursy!</h1>
-      <button onClick={showCreateFormHandler}>{showCreateForm ? "Zamknij" : "Dodaj kurs"}</button>
-      {showCreateForm && createForm}
-      <hr/>
-      {coursesData ? (
-        coursesData.courses.map((item) => (
-          <div key={item.id} className={classes.course}>
-            <Card>
-              <div
-                onClick={() => {
-                  courseDetailsHandler(item.id);
-                }}
-              >
-                <h2>{item.name}</h2>
-                <div>{item.category}</div>
+    <>
+      {/* <SideNavigation /> */}
+      <section className={classes["my-courses"]}>
+        <h1>Twoje kursy!</h1>
+        <button onClick={showCreateFormHandler}>
+          {showCreateForm ? "Zamknij" : "Dodaj kurs"}
+        </button>
+        {showCreateForm && createForm}
+        <div className={classes["courses"]}>
+          {coursesData ? (
+            coursesData.courses.map((item) => (
+              <div key={item.id} className={classes["course"]}>
+                <div>
+                  <FaTimes size={25} onClick={() => {
+                      if (
+                        window.confirm(
+                          `Czy napewno chcesz usunąć ${item.name}?`
+                        )
+                      )
+                        deleteCourseRequest(item.id);
+                    }} className={classes["delete-icon"]} />
+                  <div
+                    onClick={() => {
+                      courseDetailsHandler(item.id);
+                    }}
+                  >
+                    <div className={classes["course__image-container"]}>
+                      <img src={image} alt="image" />
+                    </div>
+                    <h2 className={classes["course-description__title"]}>
+                      {item.name}
+                    </h2>
+                    <div
+                      className={
+                        classes["course-description__category-container"]
+                      }
+                    >
+                      <span className={classes["category-title"]}>
+                        
+                      </span>
+                      <span className={classes["category-value"]}>
+                        {item.category}
+                      </span>
+                    </div>
+                  </div>
+                  {/* <button
+                    onClick={() => {
+                      if (
+                        window.confirm(
+                          `Czy napewno chcesz usunąć ${item.name}?`
+                        )
+                      )
+                        deleteCourseRequest(item.id);
+                    }}
+                  >
+                    Usuń kurs
+                  </button> */}
+                </div>
               </div>
-              <button
-                onClick={() => {
-                  if (window.confirm(`Czy napewno chcesz usunąć ${item.name}?`))
-                    deleteCourseRequest(item.id);
-                }}
-              >
-                Usuń kurs
-              </button>
+            ))
+          ) : courseError ? (
+            <div className={classes["error"]}>
+              <h1>{courseError}</h1>
+            </div>
+          ) : (
+            <Card>
+              <div>
+                <LoadingSpinner />
+              </div>
             </Card>
-          </div>
-        ))
-      ) : ( courseError ? <div className={classes.error}><h1>{courseError}</h1></div> :
-        <Card>
-          <div>
-            <LoadingSpinner />
-          </div>
-        </Card>
-      )}
-    </section>
+          )}
+        </div>
+      </section>
+    </>
   );
 };
 
