d5e94bee54f38cfc848df05800304836703c94f2
hubert123490 <hubert1234.94@gmail.com>
2021-12-24 23:01:57 +0100

    App component refactor

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import StartingPageContent from "./components/StartingPage/StartingPage";
 import ProfilePage from "./pages/ProfilePage";
-import { useContext, useState, useEffect } from "react";
+import { useContext } from "react";
 import AuthContext from "./store/auth-context";
 import AuthFormPage from "./pages/AuthFormPage";
 import TeacherPage from "./pages/TeacherPage";
@@ -12,24 +12,7 @@ import StudentPage from "./pages/StudentPage";
 
 function App() {
   const authCtx = useContext(AuthContext);
-
-  const [user] = useState(JSON.parse(localStorage.getItem("user")));
-  const [showStudentContent, setShowStudentContent] = useState(false);
-  const [showTeacherContent, setShowTeacherContent] = useState(false);
-
   const isLoggedIn = authCtx.isLoggedIn;
-
-  useEffect(() => {
-    if (user) {
-      if (user.roles.includes("ROLE_STUDENT")) {
-        setShowStudentContent(true);
-      }
-
-      if (user.roles.includes("ROLE_TEACHER")) {
-        setShowTeacherContent(true);
-      }
-    }
-  }, [user]);
 
   return (
     <Layout>
