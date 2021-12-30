884b9699c1aa67d4edb828089e56b3a7df0aa354
hubert123490 <hubert1234.94@gmail.com>
2021-12-30 17:26:52 +0100

    fixed LoadingSpinner bug while filtering

--- src/components/StartingPage/Courses.js
+++ src/components/StartingPage/Courses.js
@@ -1,52 +1,69 @@
 import classes from "./Courses.module.css";
 import image from "../../assets/course_image.jpg";
-import Card from "../UI/Card";
 import LoadingSpinner from "../UI/LoadingSpinner";
 
-const Courses = ({ data, courseDetailsHandler, getTitle }) => {
+const Courses = ({ data, courseDetailsHandler, getTitle, status }) => {
+  console.log(status);
+
   return (
-    <div className={classes["courses"]}>
-      {data ? (
-        data.map((item) => (
-          <div
-            key={item.id}
-            onClick={() => courseDetailsHandler(item.id)}
-            className={classes["course"]}
-          >
-            <div>
-              <div className={classes["course-description__image-container"]}>
-                <img src={image} alt="course" />
-              </div>
-              <h2 className={classes["course-description__title"]}>
-                {item.name}
-              </h2>
+    <>
+      {data && data.length !== 0 && status === "completed" && (
+        <div className={classes["courses"]}>
+          {data &&
+            status === "completed" &&
+            data.map((item) => (
               <div
-                className={classes["course-description__category-container"]}
+                key={item.id}
+                onClick={() => courseDetailsHandler(item.id)}
+                className={classes["course"]}
               >
-                <span className={classes["category-title"]}></span>
-                <span className={classes["category-value"]}>
-                  {item.category}
-                </span>
-              </div>
-              {item.person.map((person) => (
-                <div
-                  className={classes["course-description__teacher-container"]}
-                  key={person.id}
-                >
-                  {getTitle(person.title)} {person.lastName} {person.firstName}
+                <div>
+                  <div
+                    className={classes["course-description__image-container"]}
+                  >
+                    <img src={image} alt="course" />
+                  </div>
+                  <h2 className={classes["course-description__title"]}>
+                    {item.name}
+                  </h2>
+                  <div
+                    className={
+                      classes["course-description__category-container"]
+                    }
+                  >
+                    <span className={classes["category-title"]}></span>
+                    <span className={classes["category-value"]}>
+                      {item.category}
+                    </span>
+                  </div>
+                  {item.person.map((person) => (
+                    <div
+                      className={
+                        classes["course-description__teacher-container"]
+                      }
+                      key={person.id}
+                    >
+                      {getTitle(person.title)} {person.lastName}{" "}
+                      {person.firstName}
+                    </div>
+                  ))}
                 </div>
-              ))}
-            </div>
-          </div>
-        ))
-      ) : (
-        <Card>
-          <div>
-            <LoadingSpinner />
-          </div>
-        </Card>
+              </div>
+            ))}
+        </div>
       )}
-    </div>
+      {data && data.length === 0 && (
+        <div className={classes["nothing-found"]}>
+          {status === "completed" && (
+            <div>
+              <h1>Nic nie znaleziono</h1>
+            </div>
+          )}
+          <br/>
+          <LoadingSpinner />
+        </div>
+      )}
+    </>
   );
 };
 
