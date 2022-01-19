63f420813bfc94b5bf5e51b2a2e26df2bf8025b3
hubert123490 <hubert1234.94@gmail.com>
2022-01-19 11:18:02 +0100

    better Profile styling

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -7,28 +7,30 @@ const Profile = () => {
   return (
     <div className={classes.profile}>
       <h1>Twój profil</h1>
-      <div>
-        <span className={classes.label}>Imię: </span>
-        {user.firstName}
-      </div>
-      <div>
-        <span className={classes.label}>Nazwisko: </span>
-        {user.lastName}
-      </div>
-      <div>
-        <span className={classes.label}>Email: </span>
-        {user.email}
-      </div>
-      <div>
-        <span className={classes.label}>Tytuł naukowy: </span>
-        {user.title === "Student" ? <>Uczeń</> : ""}
-        {user.title === "Professor" ? <>Profesor</> : ""}
-        {user.title === "Bachelor" ? <>Inżynier</> : ""}
-        {user.title === "Master" ? <>Magister</> : ""}
-      </div>
-      <div>
-        <span className={classes.label}>Rola w systemie: </span>
-        {user.roles.map((role) => role)}
+      <div className={classes["profile-details"]}>
+        <div>
+          <span className={classes.label}>Imię: </span>
+          {user.firstName}
+        </div>
+        <div>
+          <span className={classes.label}>Nazwisko: </span>
+          {user.lastName}
+        </div>
+        <div>
+          <span className={classes.label}>Email: </span>
+          {user.email}
+        </div>
+        <div>
+          <span className={classes.label}>Tytuł naukowy: </span>
+          {user.title === "Student" ? <>Uczeń</> : ""}
+          {user.title === "Professor" ? <>Profesor</> : ""}
+          {user.title === "Bachelor" ? <>Inżynier</> : ""}
+          {user.title === "Master" ? <>Magister</> : ""}
+        </div>
+        <div>
+          <span className={classes.label}>Rola w systemie: </span>
+          {user.roles.map((role) => role)}
+        </div>
       </div>
     </div>
   );
